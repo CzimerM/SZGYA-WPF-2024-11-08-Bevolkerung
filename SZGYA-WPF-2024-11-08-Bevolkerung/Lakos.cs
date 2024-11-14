461913d76b05e9b82c27df7df89b9455ce45047d
C20CsizmadiaM <C20CsizmadiaM@204-12>
2024-11-14 09:49:07 +0100

    Add project files.

---
+++ SZGYA-WPF-2024-11-08-Bevolkerung/Lakos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_WPF_2024_11_08_Bevolkerung
+{
+    internal class Lakos
+    {
+        public int Id { get; set; }
+        public string Nem { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+        public bool Dohanyzik { get; set; }
+        public string Nemzetiseg { get; set; }
+        public string Nepcsoport { get; set; }
+        public string Tartomany { get; set; }
+        public int NettoJovedelem { get; set; }
+        public string IskolaiVegzettseg { get; set; }
+        public string PolitikaiNezet { get; set; }
+        public bool AktivSzavazo { get; set; }
+        public int SorFogyasztasEvente { get; set; }
+        public int KrumpliFogyasztasEvente { get; set; }
+
+        public int Eletkor => DateTime.Now.Year - this.SzuletesiEv;
+
+        public Lakos(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Id = int.Parse(adatok[0]);
+            this.Nem = adatok[1];
+            this.SzuletesiEv = int.Parse(adatok[2]);
+            this.Suly = int.Parse(adatok[3]);
+            this.Magassag = int.Parse(adatok[4]);
+            this.Dohanyzik = adatok[5] == "igen";
+            this.Nemzetiseg = adatok[6];
+            this.Nepcsoport = adatok[7];
+            this.Tartomany = adatok[8];
+            this.NettoJovedelem = int.Parse(adatok[9]);
+            this.IskolaiVegzettseg = adatok[10].Trim().Length != 0 ? adatok[10] : null;
+            this.PolitikaiNezet = adatok[11];
+            this.AktivSzavazo = adatok[12] == "igen";
+            this.SorFogyasztasEvente = adatok[13] == "NA" ? -1 : int.Parse(adatok[13]);
+            this.KrumpliFogyasztasEvente = adatok[14] == "NA" ? -1 : int.Parse(adatok[14]);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id} {this.Nem} {this.SzuletesiEv} {this.Suly} {this.Magassag} {this.Dohanyzik} {this.Nemzetiseg} {this.Nepcsoport} {this.Tartomany} {this.NettoJovedelem} {this.IskolaiVegzettseg} {this.PolitikaiNezet} {this.AktivSzavazo} {this.SorFogyasztasEvente} {this.KrumpliFogyasztasEvente}";
+        }
+    }
+}
---
+++ SZGYA-WPF-2024-11-08-Bevolkerung/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SZGYA_WPF_2024_11_08_Bevolkerung
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Lakos> lakosok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            lakosok = new();
+            var sr = new StreamReader("../../../src/bevölkerung.txt", Encoding.UTF8);
+            _ = sr.ReadLine(); // fejléc
+            while(!sr.EndOfStream)
+            {
+                lakosok.Add(new Lakos(sr.ReadLine()));
+            }
+            sr.Close();
+            lblListaSorMennyiseg.Content = lakosok.Count;
+            lblListaElsoSor.Content = lakosok[0];
+        }
+    }
+}
